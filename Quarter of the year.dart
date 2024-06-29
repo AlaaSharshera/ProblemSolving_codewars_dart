b74175c71029a3b13bdd094c0982d0882456f586
alaa <alaasharshera123@gmail.com>
2024-06-29 23:51:18 +0300

    int quarter(int month){

---
+++ Quarter of the year.dart
@@ -0,0 +1,18 @@
+int quarter(int month) {
+  if (month > 0 && month <= 12) {
+    if (month <= 3) {
+      return 1;
+    } else if (month <= 6) {
+      return 2;
+    } else if (month <= 9) {
+      return 3;
+    } else {
+      return 4;
+    }
+  }
+  return 0;
+}
+
+void main() {
+  print(quarter(8));
+}
